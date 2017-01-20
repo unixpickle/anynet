a3285a571937b01c8479c99e0a6e50499165386d
Alex Nichol <unixpickle@gmail.com>
2017-01-20 17:20:57 -0500

    created (broken) MNIST demo
    For some reason, this is not learning very fast (if at all). I know it should be learning faster than this with this exact setup.

--- demo/mnist/main.go
+++ demo/mnist/main.go
@@ -1,25 +1,56 @@
 package main
 
 import (
+	"log"
+
+	"github.com/unixpickle/anydiff"
 	"github.com/unixpickle/anynet"
 	"github.com/unixpickle/anynet/anyff"
+	"github.com/unixpickle/anynet/anysgd"
 	"github.com/unixpickle/anyvec"
 	"github.com/unixpickle/anyvec/anyvec32"
 	"github.com/unixpickle/mnist"
+	"github.com/unixpickle/rip"
 )
 
 var Creator anyvec.Creator
 
 func main() {
+	log.Println("Setting up...")
+
 	Creator = anyvec32.CurrentCreator()
 
-	samples := trainingSet()
 	network := anynet.Net{
 		anynet.NewFC(Creator, 28*28, 300),
 		anynet.Tanh,
 		anynet.NewFC(Creator, 300, 10),
 		anynet.LogSoftmax,
 	}
+
+	g := &anyff.Gradienter{
+		Net:    network,
+		Cost:   anynet.DotCost{},
+		Params: network.Parameters(),
+	}
+
+	var iterNum int
+	s := &anysgd.SGD{
+		Gradienter:  g,
+		Transformer: &anysgd.Adam{},
+		Samples:     trainingSet(),
+		Rater:       anysgd.ConstRater(0.001),
+		StatusFunc: func(s anysgd.SampleList) {
+			log.Printf("iter %d: cost=%f", iterNum, g.LastCost)
+			iterNum++
+		},
+		BatchSize: 100,
+	}
+
+	log.Println("Press ctrl+c once to stop...")
+	s.Run(rip.NewRIP())
+
+	log.Println("Computing statistics...")
+	printStats(network)
 }
 
 func trainingSet() anyff.SampleList {
@@ -37,4 +68,16 @@ func trainingSet() anyff.SampleList {
 	}
 
 	return slice
+}
+
+func printStats(net anynet.Net) {
+	ts := mnist.LoadTestingDataSet()
+	cf := func(in []float64) int {
+		vec := Creator.MakeVectorData(Creator.MakeNumericList(in))
+		inRes := anydiff.NewConst(vec)
+		res := net.Apply(inRes, 1).Output()
+		return anyvec.MaxIndex(res)
+	}
+	log.Println("Validation:", ts.NumCorrect(cf))
+	log.Println("Histogram:", ts.CorrectnessHistogram(cf))
 }
