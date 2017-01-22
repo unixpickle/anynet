4246beadd20485861c1d67b19241d8a2e90007cf
Alex Nichol <unixpickle@gmail.com>
2017-01-21 22:58:17 -0500

    add special SortableSampleList type
    Otherwise, a SortSampleList essentially forced the sample list to produce every single sample at every epoch of SGD. If a sample list uses lazy evaluation to produce samples, doing something like that could take a long while.

--- anys2s/samples.go
+++ anys2s/samples.go
@@ -46,12 +46,21 @@ func (s SliceSampleList) GetSample(idx int) *Sample {
 	return s[idx]
 }
 
+// A SortableSampleList is a SampleList with an extra
+// LenAt method for efficiently getting the length of an
+// input sequence.
+type SortableSampleList interface {
+	SampleList
+
+	LenAt(idx int) int
+}
+
 // A SortSampleList wraps a SampleList and ensures that
 // samples will be sorted within reasonably small chunks.
 // This is often beneficial for RNNs on a GPU, since it
 // helps to keep batch sizes stable across timesteps.
 type SortSampleList struct {
-	SampleList
+	SortableSampleList
 
 	// BatchSize is the size of the chunks that should be
 	// sorted.
@@ -60,9 +69,10 @@ type SortSampleList struct {
 
 // Slice produces a subset of the SortSampleList.
 func (s *SortSampleList) Slice(i, j int) anysgd.SampleList {
+	sliced := s.SortableSampleList.Slice(i, j)
 	return &SortSampleList{
-		SampleList: s.SampleList.Slice(i, j).(SampleList),
-		BatchSize:  s.BatchSize,
+		SortableSampleList: sliced.(SortableSampleList),
+		BatchSize:          s.BatchSize,
 	}
 }
 
@@ -73,13 +83,13 @@ func (s *SortSampleList) PostShuffle() {
 		if bs > s.Len()-i {
 			bs = s.Len() - i
 		}
-		s := &sorter{S: s.SampleList, Start: i, End: i + bs}
+		s := &sorter{S: s.SortableSampleList, Start: i, End: i + bs}
 		sort.Sort(s)
 	}
 }
 
 type sorter struct {
-	S     SampleList
+	S     SortableSampleList
 	Start int
 	End   int
 }
@@ -93,5 +103,5 @@ func (s *sorter) Swap(i, j int) {
 }
 
 func (s *sorter) Less(i, j int) bool {
-	return len(s.S.GetSample(i+s.Start).Input) < len(s.S.GetSample(j+s.Start).Input)
+	return s.S.LenAt(i+s.Start) < s.S.LenAt(j+s.Start)
 }
