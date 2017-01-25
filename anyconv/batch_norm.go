856ff4b0fb1ab9550947fe767c98dcb7704748cc
Alex Nichol <unixpickle@gmail.com>
2017-01-24 20:22:54 -0500

    implemented batch normalization

---
+++ anyconv/batch_norm.go
@@ -0,0 +1,126 @@
+package anyconv
+
+import (
+	"github.com/unixpickle/anydiff"
+	"github.com/unixpickle/anyvec"
+	"github.com/unixpickle/anyvec/anyvecsave"
+	"github.com/unixpickle/serializer"
+)
+
+const defaultBNStabilizer = 1e-3
+
+func init() {
+	var b BatchNorm
+	serializer.RegisterTypedDeserializer(b.SerializerType(), DeserializeBatchNorm)
+}
+
+// BatchNorm is a batch normalization layer.
+//
+// After a network has finished training, BatchNorm layers
+// will typically be replaced with anynet.Affine layers so
+// that the normalizations are less noisy.
+type BatchNorm struct {
+	// InputCount indicates how many components to noramlize.
+	//
+	// For use after a fully-connected layer, this should be
+	// the total number of output neurons.
+	// For use after a convolutional layer, this sholud be
+	// the number of filters.
+	InputCount int
+
+	// Post-normalization affine transform.
+	Scalers *anydiff.Var
+	Biases  *anydiff.Var
+
+	// Stabilizer prevents numerical instability by adding a
+	// small constant to variances to keep them from being 0.
+	//
+	// If it is 0, a default is used.
+	Stabilizer float64
+}
+
+// DeserializeBatchNorm deserializes a BatchNorm.
+func DeserializeBatchNorm(d []byte) (*BatchNorm, error) {
+	var s, b *anyvecsave.S
+	var stab serializer.Float64
+	if err := serializer.DeserializeAny(d, &s, &b, &stab); err != nil {
+		return nil, err
+	}
+	return &BatchNorm{
+		InputCount: s.Vector.Len(),
+		Scalers:    anydiff.NewVar(s.Vector),
+		Biases:     anydiff.NewVar(b.Vector),
+		Stabilizer: float64(stab),
+	}, nil
+}
+
+// NewBatchNorm creates a BatchNorm with an input size.
+func NewBatchNorm(c anyvec.Creator, inCount int) *BatchNorm {
+	oneScaler := c.MakeVector(inCount)
+	oneScaler.AddScaler(c.MakeNumeric(1))
+	return &BatchNorm{
+		InputCount: inCount,
+		Scalers:    anydiff.NewVar(oneScaler),
+		Biases:     anydiff.NewVar(c.MakeVector(inCount)),
+	}
+}
+
+// Apply applies the layer to some inputs.
+func (b *BatchNorm) Apply(in anydiff.Res, batch int) anydiff.Res {
+	return anydiff.Pool(in, func(in anydiff.Res) anydiff.Res {
+		c := in.Output().Creator()
+
+		mean := b.computeMean(in)
+		secondMoment := b.computeMean(anydiff.Square(in))
+		variance := anydiff.Sub(secondMoment, anydiff.Square(mean))
+
+		stab := b.Stabilizer
+		if stab == 0 {
+			stab = defaultBNStabilizer
+		}
+		variance = anydiff.AddScaler(variance, c.MakeNumeric(stab))
+		normalizer := anydiff.Pow(variance, c.MakeNumeric(-0.5))
+
+		normalized := anydiff.ScaleRepeated(
+			anydiff.AddRepeated(in, anydiff.Scale(mean, c.MakeNumeric(-1))),
+			normalizer,
+		)
+
+		return anydiff.ScaleAddRepeated(normalized, b.Scalers, b.Biases)
+	})
+}
+
+// Parameters returns a slice containing the scales and
+// biases, in that order.
+func (b *BatchNorm) Parameters() []*anydiff.Var {
+	return []*anydiff.Var{b.Scalers, b.Biases}
+}
+
+// SerializerType returns the unique ID used to serialize
+// a BatchNorm with the serializer package.
+func (b *BatchNorm) SerializerType() string {
+	return "github.com/unixpickle/anynet/anyconv.BatchNorm"
+}
+
+// Serialize serializes the layer.
+func (b *BatchNorm) Serialize() ([]byte, error) {
+	return serializer.SerializeAny(
+		&anyvecsave.S{Vector: b.Scalers.Vector},
+		&anyvecsave.S{Vector: b.Biases.Vector},
+		serializer.Float64(b.Stabilizer),
+	)
+}
+
+func (b *BatchNorm) computeMean(in anydiff.Res) anydiff.Res {
+	if in.Output().Len()%b.InputCount != 0 {
+		panic("invalid input size")
+	}
+	n := in.Output().Len() / b.InputCount
+	summation := anydiff.SumRows(&anydiff.Matrix{
+		Data: in,
+		Rows: n,
+		Cols: b.InputCount,
+	})
+	scaler := 1 / float64(n)
+	return anydiff.Scale(summation, in.Output().Creator().MakeNumeric(scaler))
+}
---
+++ anyconv/batch_norm_test.go
@@ -0,0 +1,49 @@
+package anyconv
+
+import (
+	"math"
+	"testing"
+
+	"github.com/unixpickle/anydiff"
+	"github.com/unixpickle/anydiff/anydifftest"
+	"github.com/unixpickle/anyvec"
+	"github.com/unixpickle/anyvec/anyvec32"
+)
+
+func TestBatchNormOutput(t *testing.T) {
+	layer := &BatchNorm{
+		InputCount: 2,
+		Scalers:    anydiff.NewVar(anyvec32.MakeVectorData([]float32{2, -3})),
+		Biases:     anydiff.NewVar(anyvec32.MakeVectorData([]float32{-1.5, 2})),
+	}
+	vec := anyvec32.MakeVectorData([]float32{
+		-0.636299517987754, 1.381820934572628, 1.117062796520384,
+		-1.032042307499387, -0.603144099627179, 0.937477768422949,
+	})
+	actual := layer.Apply(anydiff.NewConst(vec), 1).Output().Data().([]float32)
+	expected := []float32{
+		-2.953427612694010, -0.723517206628873, 1.325934113323319,
+		6.176822169129221, -2.872506500629310, 0.546695037499651,
+	}
+	for i, x := range expected {
+		a := actual[i]
+		if math.IsNaN(float64(a)) || math.Abs(float64(a-x)) > 1e-3 {
+			t.Fatalf("expected %v but got %v", expected, actual)
+		}
+	}
+}
+
+func TestBatchNormProp(t *testing.T) {
+	layer := NewBatchNorm(anyvec32.CurrentCreator(), 2)
+	input := anyvec32.MakeVector(24)
+	anyvec.Rand(input, anyvec.Normal, nil)
+	inVar := anydiff.NewVar(input)
+
+	checker := anydifftest.ResChecker{
+		F: func() anydiff.Res {
+			return layer.Apply(inVar, 12)
+		},
+		V: []*anydiff.Var{inVar},
+	}
+	checker.FullCheck(t)
+}
