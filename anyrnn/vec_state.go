4ed19e84d0306a79171758f07077c8800a4de1b7
Alex Nichol <unixpickle@gmail.com>
2017-02-07 12:11:17 -0500

    use anyseq reduction/expansion API in anyrnn

--- anyrnn/vec_state.go
+++ anyrnn/vec_state.go
@@ -2,6 +2,7 @@ package anyrnn
 
 import (
 	"github.com/unixpickle/anydiff"
+	"github.com/unixpickle/anydiff/anyseq"
 	"github.com/unixpickle/anyvec"
 )
 
@@ -35,69 +36,17 @@ func (v *VecState) Present() PresentMap {
 // Reduce generates a new *VecState with a subset of the
 // chunks in v.
 func (v *VecState) Reduce(p PresentMap) State {
-	n := v.PresentMap.NumPresent()
-	inc := v.Vector.Len() / n
-
-	var chunks []anyvec.Vector
-	var chunkStart, chunkSize int
-	for i, pres := range p {
-		if pres {
-			if !v.PresentMap[i] {
-				panic("argument to Reduce must be a subset")
-			}
-			chunkSize += inc
-		} else if v.PresentMap[i] {
-			if chunkSize > 0 {
-				chunks = append(chunks, v.Vector.Slice(chunkStart, chunkStart+chunkSize))
-				chunkStart += chunkSize
-				chunkSize = 0
-			}
-			chunkStart += inc
-		}
-	}
-	if chunkSize > 0 {
-		chunks = append(chunks, v.Vector.Slice(chunkStart, chunkStart+chunkSize))
-	}
-
-	return &VecState{
-		Vector:     v.Vector.Creator().Concat(chunks...),
-		PresentMap: p,
-	}
+	b := &anyseq.Batch{Packed: v.Vector, Present: v.PresentMap}
+	res := anyseq.ReduceBatch(b, p)
+	return &VecState{Vector: res.Packed, PresentMap: p}
 }
 
 // Expand expands the *VecState by inserting zero chunks
 // where necessary, producing a new *VecState.
 func (v *VecState) Expand(p PresentMap) StateGrad {
-	n := v.PresentMap.NumPresent()
-	inc := v.Vector.Len() / n
-	filler := v.Vector.Creator().MakeVector(inc)
-
-	var chunks []anyvec.Vector
-	var chunkStart, chunkSize int
-
-	for i, pres := range p {
-		if v.PresentMap[i] {
-			if !pres {
-				panic("argument to Expand must be a superset")
-			}
-			chunkSize += inc
-		} else if pres {
-			if chunkSize > 0 {
-				chunks = append(chunks, v.Vector.Slice(chunkStart, chunkStart+chunkSize))
-				chunkStart += chunkSize
-				chunkSize = 0
-			}
-			chunks = append(chunks, filler)
-		}
-	}
-	if chunkSize > 0 {
-		chunks = append(chunks, v.Vector.Slice(chunkStart, chunkSize+chunkStart))
-	}
-
-	return &VecState{
-		Vector:     v.Vector.Creator().Concat(chunks...),
-		PresentMap: p,
-	}
+	b := &anyseq.Batch{Packed: v.Vector, Present: v.PresentMap}
+	res := anyseq.ExpandBatch(b, p)
+	return &VecState{Vector: res.Packed, PresentMap: p}
 }
 
 // PropagateStart propagates the contents of the vector,
