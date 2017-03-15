43c6f7233c8f26d7d08d720a2371102897fad6ba
Alex Nichol <unixpickle@gmail.com>
2017-03-14 21:18:25 -0400

    use convmarkup Realizer API
    This will make it easier to extend the markup language while still leveraging the convmarkup implementation in anyconv.

--- anyconv/markup.go
+++ anyconv/markup.go
@@ -24,151 +24,129 @@ func FromMarkup(c anyvec.Creator, code string) (anynet.Layer, error) {
 	if err != nil {
 		return nil, errors.New("make markup block: " + err.Error())
 	}
-	return FromMarkupBlock(c, block, convmarkup.Dims{})
-}
-
-// FromMarkupBlock creates a neural network from a parsed
-// markup block.
-//
-// If the block is a root block, then inDims is typically
-// going to be ignored.
-func FromMarkupBlock(c anyvec.Creator, b convmarkup.Block, inDims convmarkup.Dims) (anynet.Layer, error) {
-	switch b := b.(type) {
-	case *convmarkup.Root:
-		return netForChildren(inDims, c, b.Children)
-	case *convmarkup.Conv:
-		return layerForConvBlock(inDims, c, b)
-	case *convmarkup.Residual:
-		return layerForResidualBlock(inDims, c, b)
-	case *convmarkup.FC:
-		return anynet.NewFC(c, inDims.Width*inDims.Height*inDims.Depth, b.OutCount), nil
-	case *convmarkup.Activation:
-		return layerForActivationBlock(inDims, c, b)
-	case *convmarkup.Pool:
-		if b.Name == "MeanPool" {
-			return &MeanPool{
-				SpanX:       b.Width,
-				SpanY:       b.Height,
-				StrideX:     b.StrideX,
-				StrideY:     b.StrideY,
-				InputWidth:  inDims.Width,
-				InputHeight: inDims.Height,
-				InputDepth:  inDims.Depth,
-			}, nil
-		} else {
-			return &MaxPool{
-				SpanX:       b.Width,
-				SpanY:       b.Height,
-				StrideX:     b.StrideX,
-				StrideY:     b.StrideY,
-				InputWidth:  inDims.Width,
-				InputHeight: inDims.Height,
-				InputDepth:  inDims.Depth,
-			}, nil
-		}
-	case *convmarkup.Padding:
-		return &Padding{
-			InputWidth:    inDims.Width,
-			InputHeight:   inDims.Height,
-			InputDepth:    inDims.Depth,
-			PaddingTop:    b.Top,
-			PaddingRight:  b.Right,
-			PaddingBottom: b.Bottom,
-			PaddingLeft:   b.Left,
-		}, nil
-	case *convmarkup.Resize:
-		return &Resize{
-			InputWidth:   inDims.Width,
-			InputHeight:  inDims.Height,
-			Depth:        inDims.Depth,
-			OutputWidth:  b.Out.Width,
-			OutputHeight: b.Out.Height,
-		}, nil
-	case *convmarkup.Linear:
-		scalerVec := c.MakeVector(1)
-		scalerVec.AddScaler(c.MakeNumeric(b.Scale))
-		biasVec := c.MakeVector(1)
-		biasVec.AddScaler(c.MakeNumeric(b.Bias))
-		return &anynet.ParamHider{
-			Layer: &anynet.Affine{
-				Scalers: anydiff.NewVar(scalerVec),
-				Biases:  anydiff.NewVar(biasVec),
-			},
-		}, nil
-	default:
-		return nil, fmt.Errorf("unexpected markup block: %s", b.Type())
+	chain := convmarkup.RealizerChain{convmarkup.MetaRealizer{},
+		Realizer(c)}
+	instance, err := chain.Realize(convmarkup.Dims{}, block)
+	if err != nil {
+		return nil, errors.New("realize markup block: " + err.Error())
+	}
+	if layer, ok := instance.(anynet.Layer); ok {
+		return layer, nil
+	} else {
+		return nil, fmt.Errorf("bad markup block type: %T", instance)
 	}
 }
 
-func netForChildren(inDims convmarkup.Dims, c anyvec.Creator,
+// Realizer creates a convmarkup.Realizer capable of
+// realizing convolutional networks, residual layers, etc.
+//
+// The Realizer is meant to be used in conjunction with an
+// anyconv.MetaRealizer.
+func Realizer(c anyvec.Creator) convmarkup.Realizer {
+	return &realizer{creator: c}
+}
+
+type realizer struct {
+	creator anyvec.Creator
+}
+
+func (r *realizer) Realize(chain convmarkup.RealizerChain, inDims convmarkup.Dims,
+	b convmarkup.Block) (interface{}, error) {
+	switch b := b.(type) {
+	case *convmarkup.Root:
+		return r.net(chain, inDims, b.Children)
+	case *convmarkup.Conv:
+		return r.conv(inDims, b)
+	case *convmarkup.Residual:
+		return r.residual(chain, inDims, b)
+	case *convmarkup.FC:
+		return r.fc(inDims, b)
+	case *convmarkup.Activation:
+		return r.activation(inDims, b)
+	case *convmarkup.Pool:
+		return r.pool(inDims, b)
+	case *convmarkup.Padding:
+		return r.padding(inDims, b)
+	case *convmarkup.Resize:
+		return r.resize(inDims, b)
+	case *convmarkup.Linear:
+		return r.linear(b)
+	default:
+		return nil, convmarkup.ErrUnsupportedBlock
+	}
+}
+
+func (r *realizer) net(chain convmarkup.RealizerChain, inDims convmarkup.Dims,
 	ch []convmarkup.Block) (anynet.Net, error) {
 	var res anynet.Net
-	for _, x := range ch {
-		if _, ok := x.(*convmarkup.Input); ok {
-			inDims = x.OutDims()
-			continue
-		} else if _, ok = x.(*convmarkup.Assert); ok {
-			continue
-		} else if rep, ok := x.(*convmarkup.Repeat); ok {
+	for _, b := range ch {
+		// Avoiding nested anynet.Net objects.
+		if rep, ok := b.(*convmarkup.Repeat); ok {
 			for i := 0; i < rep.N; i++ {
-				net, err := netForChildren(inDims, c, rep.Children)
+				net, err := r.net(chain, inDims, rep.Children)
 				if err != nil {
 					return nil, err
 				}
 				res = append(res, net...)
 			}
-		} else {
-			layer, err := FromMarkupBlock(c, x, inDims)
-			if err != nil {
-				return nil, err
-			}
-			res = append(res, layer)
-			inDims = x.OutDims()
+			continue
 		}
+		obj, err := chain.Realize(inDims, b)
+		if err != nil {
+			return nil, err
+		} else if obj != nil {
+			if layer, ok := obj.(anynet.Layer); ok {
+				res = append(res, layer)
+			} else {
+				return nil, fmt.Errorf("unexpected instance type: %T", obj)
+			}
+		}
+		inDims = b.OutDims()
 	}
 	return res, nil
 }
 
-func layerForConvBlock(inDims convmarkup.Dims, c anyvec.Creator,
-	b *convmarkup.Conv) (anynet.Layer, error) {
+func (r *realizer) conv(d convmarkup.Dims, b *convmarkup.Conv) (anynet.Layer, error) {
 	res := &Conv{
 		FilterWidth:  b.FilterWidth,
 		FilterHeight: b.FilterHeight,
 		FilterCount:  b.FilterCount,
 		StrideX:      b.StrideX,
 		StrideY:      b.StrideY,
-		InputWidth:   inDims.Width,
-		InputHeight:  inDims.Height,
-		InputDepth:   inDims.Depth,
+		InputWidth:   d.Width,
+		InputHeight:  d.Height,
+		InputDepth:   d.Depth,
 	}
-	res.InitRand(c)
+	res.InitRand(r.creator)
 	return res, nil
 }
 
-func layerForResidualBlock(inDims convmarkup.Dims, c anyvec.Creator,
+func (r *realizer) residual(chain convmarkup.RealizerChain, d convmarkup.Dims,
 	b *convmarkup.Residual) (anynet.Layer, error) {
-	resPart, err := netForChildren(inDims, c, b.Residual)
+	resPart, err := r.net(chain, d, b.Residual)
 	if err != nil {
 		return nil, err
 	}
 	res := &Residual{Layer: resPart}
-
 	if len(b.Projection) != 0 {
-		projPart, err := netForChildren(inDims, c, b.Projection)
+		projPart, err := r.net(chain, d, b.Projection)
 		if err != nil {
 			return nil, err
 		}
 		res.Projection = projPart
 	}
-
 	return res, nil
 }
 
-func layerForActivationBlock(inDims convmarkup.Dims, c anyvec.Creator,
-	b *convmarkup.Activation) (anynet.Layer, error) {
+func (r *realizer) fc(d convmarkup.Dims, b *convmarkup.FC) (anynet.Layer, error) {
+	inSize := d.Width * d.Height * d.Depth
+	return anynet.NewFC(r.creator, inSize, b.OutCount), nil
+}
+
+func (r *realizer) activation(d convmarkup.Dims, b *convmarkup.Activation) (anynet.Layer, error) {
 	switch b.Name {
 	case "BatchNorm":
-		return NewBatchNorm(c, inDims.Depth), nil
+		return NewBatchNorm(r.creator, d.Depth), nil
 	case "ReLU":
 		return anynet.ReLU, nil
 	case "Sigmoid":
@@ -180,4 +158,66 @@ func layerForActivationBlock(inDims convmarkup.Dims, c anyvec.Creator,
 	default:
 		return nil, fmt.Errorf("unknown activation: %s", b.Name)
 	}
+}
+
+func (r *realizer) pool(d convmarkup.Dims, b *convmarkup.Pool) (anynet.Layer, error) {
+	switch b.Name {
+	case "MeanPool":
+		return &MeanPool{
+			SpanX:       b.Width,
+			SpanY:       b.Height,
+			StrideX:     b.StrideX,
+			StrideY:     b.StrideY,
+			InputWidth:  d.Width,
+			InputHeight: d.Height,
+			InputDepth:  d.Depth,
+		}, nil
+	case "MaxPool":
+		return &MaxPool{
+			SpanX:       b.Width,
+			SpanY:       b.Height,
+			StrideX:     b.StrideX,
+			StrideY:     b.StrideY,
+			InputWidth:  d.Width,
+			InputHeight: d.Height,
+			InputDepth:  d.Depth,
+		}, nil
+	default:
+		return nil, fmt.Errorf("unknown pooling: %s", b.Name)
+	}
+}
+
+func (r *realizer) padding(d convmarkup.Dims, b *convmarkup.Padding) (anynet.Layer, error) {
+	return &Padding{
+		InputWidth:    d.Width,
+		InputHeight:   d.Height,
+		InputDepth:    d.Depth,
+		PaddingTop:    b.Top,
+		PaddingRight:  b.Right,
+		PaddingBottom: b.Bottom,
+		PaddingLeft:   b.Left,
+	}, nil
+}
+
+func (r *realizer) resize(d convmarkup.Dims, b *convmarkup.Resize) (anynet.Layer, error) {
+	return &Resize{
+		InputWidth:   d.Width,
+		InputHeight:  d.Height,
+		Depth:        d.Depth,
+		OutputWidth:  b.Out.Width,
+		OutputHeight: b.Out.Height,
+	}, nil
+}
+
+func (r *realizer) linear(b *convmarkup.Linear) (anynet.Layer, error) {
+	scalerVec := r.creator.MakeVector(1)
+	scalerVec.AddScaler(r.creator.MakeNumeric(b.Scale))
+	biasVec := r.creator.MakeVector(1)
+	biasVec.AddScaler(r.creator.MakeNumeric(b.Bias))
+	return &anynet.ParamHider{
+		Layer: &anynet.Affine{
+			Scalers: anydiff.NewVar(scalerVec),
+			Biases:  anydiff.NewVar(biasVec),
+		},
+	}, nil
 }
