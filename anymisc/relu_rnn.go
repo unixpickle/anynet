2a2b3c209eaea7603822d1251a2b1f0ff0e5ea96
Alex Nichol <unixpickle@gmail.com>
2017-04-28 16:26:06 -0400

    implemented npRNN

--- anymisc/relu_rnn.go
+++ anymisc/relu_rnn.go
@@ -12,17 +12,66 @@ import (
 // This is based on https://arxiv.org/abs/1504.00941.
 func NewIRNN(c anyvec.Creator, in, out int, scale float64) *anyrnn.Vanilla {
 	res := anyrnn.NewVanilla(c, in, out, anynet.ReLU)
+	res.StateWeights.Vector.Set(identityMatrix(c, out, scale).Data)
+	return res
+}
 
-	hiddenIndices := make([]int, out)
-	for i := range hiddenIndices {
-		hiddenIndices[i] = i * (out + 1)
+// NewNPRNN creates an RNN with ReLU activations and a
+// positive-definite transition matrix with bounded
+// eigenvalues.
+//
+// This is based on https://arxiv.org/abs/1511.03771.
+func NewNPRNN(c anyvec.Creator, in, out int) *anyrnn.Vanilla {
+	res := anyrnn.NewVanilla(c, in, out, anynet.ReLU)
+
+	factor := &anyvec.Matrix{
+		Data: c.MakeVector(out * out),
+		Rows: out,
+		Cols: out,
 	}
-	mapper := c.MakeMapper(out*out, hiddenIndices)
-	diagonal := c.MakeVector(out)
-	diagonal.AddScalar(c.MakeNumeric(scale))
+	anyvec.Rand(factor.Data, anyvec.Normal, nil)
+	posDef := identityMatrix(c, out, 1)
+	posDef.Product(true, false, c.MakeNumeric(1/float64(out)), factor, factor,
+		c.MakeNumeric(1))
 
-	res.StateWeights.Vector.Scale(c.MakeNumeric(0))
-	mapper.MapTranspose(diagonal, res.StateWeights.Vector)
+	res.StateWeights.Vector.Set(posDef.Data)
+	res.StateWeights.Vector.Scale(inverseLargestEig(posDef))
 
 	return res
+}
+
+func identityMatrix(c anyvec.Creator, size int, scale float64) *anyvec.Matrix {
+	hiddenIndices := make([]int, size)
+	for i := range hiddenIndices {
+		hiddenIndices[i] = i * (size + 1)
+	}
+	mapper := c.MakeMapper(size*size, hiddenIndices)
+	diagonal := c.MakeVector(size)
+	diagonal.AddScalar(c.MakeNumeric(scale))
+
+	res := c.MakeVector(size * size)
+	mapper.MapTranspose(diagonal, res)
+	return &anyvec.Matrix{Data: res, Rows: size, Cols: size}
+}
+
+func inverseLargestEig(mat *anyvec.Matrix) anyvec.Numeric {
+	const numIters = 100
+
+	c := mat.Data.Creator()
+	ops := c.NumOps()
+	inVec := c.MakeVector(mat.Cols)
+	outVec := c.MakeVector(mat.Cols)
+
+	anyvec.Rand(inVec, anyvec.Normal, nil)
+
+	// Power iteration method: it's slow, but it works.
+	for i := 0; i < numIters; i++ {
+		mag := anyvec.Norm(inVec)
+		inVec.Scale(ops.Div(c.MakeNumeric(1), mag))
+		anyvec.Gemv(false, mat.Rows, mat.Cols, c.MakeNumeric(1), mat.Data,
+			mat.Cols, inVec, 1, c.MakeNumeric(0), outVec, 1)
+		inVec, outVec = outVec, inVec
+	}
+
+	return ops.Div(c.MakeNumeric(1), anyvec.Norm(inVec))
 }
--- anymisc/relu_rnn_test.go
+++ anymisc/relu_rnn_test.go
@@ -4,6 +4,7 @@ import (
 	"reflect"
 	"testing"
 
+	"github.com/unixpickle/anyvec"
 	"github.com/unixpickle/anyvec/anyvec64"
 )
 
@@ -17,5 +18,34 @@ func TestIRNN(t *testing.T) {
 	})
 	if !reflect.DeepEqual(rnn.StateWeights.Vector, expectedDiag) {
 		t.Errorf("expected %v but got %v", expectedDiag, rnn.StateWeights.Vector)
+	}
+}
+
+func TestNPRNN(t *testing.T) {
+	c := anyvec64.DefaultCreator{}
+	rnn := NewNPRNN(c, 5, 4)
+	matrix := &anyvec.Matrix{
+		Data: rnn.StateWeights.Vector,
+		Rows: 4,
+		Cols: 4,
+	}
+	for i := 0; i < 30; i++ {
+		inVec := &anyvec.Matrix{
+			Data: c.MakeVector(matrix.Cols),
+			Rows: matrix.Cols,
+			Cols: 1,
+		}
+		product := &anyvec.Matrix{
+			Data: c.MakeVector(matrix.Rows),
+			Rows: matrix.Rows,
+			Cols: 1,
+		}
+		anyvec.Rand(inVec.Data, anyvec.Normal, nil)
+		inMag := anyvec.Norm(inVec.Data)
+		product.Product(false, false, c.MakeNumeric(1), matrix, inVec, c.MakeNumeric(0))
+		outMag := anyvec.Norm(product.Data)
+		if inMag.(float64) < outMag.(float64) {
+			t.Error("an eigenvalue was too big")
+		}
 	}
 }
