020bec34bac3cffa711d7719fa6d1688ffac54a5
Alex Nichol <unixpickle@gmail.com>
2017-05-19 12:59:08 -0400

    added Markov RNN block
    This has a number of different use cases. For example, you could use it in an RL setting to pass the last few frames to a convolutional neural network. You could also use it as the first layer in an NLP network to give each timestep a little more structured context.

---
+++ anyrnn/markov.go
@@ -0,0 +1,133 @@
+package anyrnn
+
+import (
+	"github.com/unixpickle/anydiff"
+	"github.com/unixpickle/anyvec"
+	"github.com/unixpickle/anyvec/anyvecsave"
+	"github.com/unixpickle/essentials"
+	"github.com/unixpickle/serializer"
+)
+
+func init() {
+	var m Markov
+	serializer.RegisterTypedDeserializer(m.SerializerType(), DeserializeMarkov)
+}
+
+// Markov is an RNN block that tracks a history of inputs.
+type Markov struct {
+	StartState *anydiff.Var
+
+	// HistorySize is the number of inputs to store.
+	// Since this does not include the input, the output
+	// will contain HistorySize+1 packed inputs.
+	HistorySize int
+
+	// DepthWise controls how vectors from the history
+	// are concatenated.
+	// If false, then vectors a and b are joined to
+	//
+	//     <a1, a2, ..., b1, b2, ...>
+	//
+	// If true, then vectors are joined depth-wise
+	//
+	//     <a1, b1, a2, b2, ...>
+	//
+	// In either case, the first component of the most
+	// recent timestep is packed before the first
+	// component of the second most recent timestep.
+	DepthWise bool
+}
+
+// NewMarkov creates a Markov with the history size and
+// pre-known input vector size.
+func NewMarkov(c anyvec.Creator, history int, inSize int) *Markov {
+	if inSize == 0 || history == 0 {
+		panic("input and history sizes must be non-zero")
+	}
+	return &Markov{
+		StartState:  anydiff.NewVar(c.MakeVector(history * inSize)),
+		HistorySize: history,
+	}
+}
+
+// DeserializeMarkov deserializes a Markov.
+func DeserializeMarkov(d []byte) (*Markov, error) {
+	var res Markov
+	var vec *anyvecsave.S
+	err := serializer.DeserializeAny(d, &vec, &res.HistorySize, &res.DepthWise)
+	if err != nil {
+		return nil, essentials.AddCtx("deserialize Markov", err)
+	}
+	res.StartState = anydiff.NewVar(vec.Vector)
+	return &res, nil
+}
+
+// Start returns a start state.
+func (m *Markov) Start(n int) State {
+	return m.funcBlock().Start(n)
+}
+
+// PropagateStart propagates through the start state.
+func (m *Markov) PropagateStart(sg StateGrad, g anydiff.Grad) {
+	m.funcBlock().PropagateStart(sg, g)
+}
+
+// Step applies the block, returning a stacked tensor and
+// updating the frame history in the state.
+func (m *Markov) Step(state State, in anyvec.Vector) Res {
+	return m.funcBlock().Step(state, in)
+}
+
+// Parameters returns the Markov's parameters.
+func (m *Markov) Parameters() []*anydiff.Var {
+	return []*anydiff.Var{m.StartState}
+}
+
+// SerializerType returns the unique ID used to serialize
+// a Markov with the serializer package.
+func (m *Markov) SerializerType() string {
+	return "github.com/unixpickle/anynet/Markov"
+}
+
+// Serialize serializes the Markov.
+func (m *Markov) Serialize() ([]byte, error) {
+	return serializer.SerializeAny(
+		&anyvecsave.S{Vector: m.StartState.Vector},
+		m.HistorySize,
+		m.DepthWise,
+	)
+}
+
+func (m *Markov) funcBlock() *FuncBlock {
+	return &FuncBlock{
+		Func: func(in, state anydiff.Res, n int) (out, newState anydiff.Res) {
+			oldRows := &anydiff.Matrix{
+				Data: state,
+				Rows: m.HistorySize,
+				Cols: state.Output().Len() / m.HistorySize,
+			}
+
+			outRows := *oldRows
+			outRows.Data = anydiff.Concat(in, oldRows.Data)
+			outRows.Rows++
+
+			stateRows := outRows
+			stateRows.Rows--
+			stateRows.Data = anydiff.Slice(outRows.Data, 0,
+				stateRows.Cols*stateRows.Rows)
+
+			if m.DepthWise {
+				return anydiff.Transpose(&outRows).Data, stateRows.Data
+			} else {
+				return outRows.Data, stateRows.Data
+			}
+		},
+		MakeStart: func(n int) anydiff.Res {
+			var rep []anydiff.Res
+			for i := 0; i < n; i++ {
+				rep = append(rep, m.StartState)
+			}
+			return anydiff.Concat(rep...)
+		},
+	}
+}
---
+++ anyrnn/markov_test.go
@@ -0,0 +1,93 @@
+package anyrnn
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/unixpickle/anydiff/anydifftest"
+	"github.com/unixpickle/anydiff/anyseq"
+	"github.com/unixpickle/anyvec"
+	"github.com/unixpickle/anyvec/anyvec64"
+	"github.com/unixpickle/serializer"
+)
+
+func TestMarkovOutput(t *testing.T) {
+	for _, mode := range []string{"Concat", "DepthWise"} {
+		t.Run(mode, func(t *testing.T) {
+			c := anyvec64.DefaultCreator{}
+			markov := NewMarkov(c, 2, 3)
+			markov.DepthWise = mode == "DepthWise"
+			markov.StartState.Vector.SetData(
+				c.MakeNumericList([]float64{-1, -2, -3, -4, -5, -6}),
+			)
+
+			inSeq := anyseq.ConstSeqList(c, [][]anyvec.Vector{
+				{
+					c.MakeVectorData(c.MakeNumericList([]float64{1, 2, 3})),
+					c.MakeVectorData(c.MakeNumericList([]float64{4, 5, 6})),
+					c.MakeVectorData(c.MakeNumericList([]float64{7, 8, 9})),
+				},
+			})
+
+			actual := anyseq.SeparateSeqs(Map(inSeq, markov).Output())
+			var expected [][]anyvec.Vector
+			if mode == "Concat" {
+				expected = [][]anyvec.Vector{
+					{
+						c.MakeVectorData(c.MakeNumericList([]float64{1, 2, 3, -1, -2, -3, -4, -5, -6})),
+						c.MakeVectorData(c.MakeNumericList([]float64{4, 5, 6, 1, 2, 3, -1, -2, -3})),
+						c.MakeVectorData(c.MakeNumericList([]float64{7, 8, 9, 4, 5, 6, 1, 2, 3})),
+					},
+				}
+			} else {
+				expected = [][]anyvec.Vector{
+					{
+						c.MakeVectorData(c.MakeNumericList([]float64{1, -1, -4, 2, -2, -5, 3, -3, -6})),
+						c.MakeVectorData(c.MakeNumericList([]float64{4, 1, -1, 5, 2, -2, 6, 3, -3})),
+						c.MakeVectorData(c.MakeNumericList([]float64{7, 4, 1, 8, 5, 2, 9, 6, 3})),
+					},
+				}
+			}
+			if !reflect.DeepEqual(actual, expected) {
+				t.Errorf("expected %#v but got %#v", expected, actual)
+			}
+		})
+	}
+}
+
+func TestMarkovGradients(t *testing.T) {
+	c := anyvec64.DefaultCreator{}
+	markov := NewMarkov(c, 2, 3)
+	markov.DepthWise = true
+	markov.StartState.Vector.SetData(
+		c.MakeNumericList([]float64{-1, -2, -3, -4, -5, -6}),
+	)
+	inSeq, inVars := randomTestSequence(c, 3)
+	checker := &anydifftest.SeqChecker{
+		F: func() anyseq.Seq {
+			return Map(inSeq, markov)
+		},
+		V: append(inVars, markov.Parameters()...),
+	}
+	checker.FullCheck(t)
+}
+
+func TestMarkovSerialize(t *testing.T) {
+	c := anyvec64.DefaultCreator{}
+	markov := NewMarkov(c, 2, 3)
+	markov.DepthWise = true
+	markov.StartState.Vector.SetData(
+		c.MakeNumericList([]float64{-1, -2, -3, -4, -5, -6}),
+	)
+	data, err := serializer.SerializeAny(markov)
+	if err != nil {
+		t.Fatal(err)
+	}
+	var markov1 *Markov
+	if err := serializer.DeserializeAny(data, &markov1); err != nil {
+		t.Fatal(err)
+	}
+	if !reflect.DeepEqual(markov, markov1) {
+		t.Error("bad deserialized value")
+	}
+}
